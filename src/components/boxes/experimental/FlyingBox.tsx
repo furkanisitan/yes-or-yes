6c9a6b3b872cbaedd97198ef72bc122207fce293
Furkan Işıtan <furkanisitandev@gmail.com>
2025-06-03 22:53:21 +0300

    Move  FlyingBox and ShakyBox components

--- src/components/boxes/experimental/FlyingBox.tsx
+++ src/components/boxes/experimental/FlyingBox.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import Box from './Box';
+import Box from '../Box';
 
 export type FlyingBoxProps = {
   label: string;
--- src/components/boxes/index.ts
+++ src/components/boxes/index.ts
@@ -1,3 +1,1 @@
 export { default as EscapeBox } from "./EscapeBox";
-export { default as FlyingBox } from "./FlyingBox";
-export { default as ShakyBox } from "./ShakyBox";
