68e68cb24c8579b54c0cb4e7a2b1d4e8ee55069e
Furkan Işıtan <furkanisitandev@gmail.com>
2025-06-04 03:44:18 +0300

    Update http-client.ts

--- src/services/http-client.ts
+++ src/services/http-client.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseURL = import.meta.env.DEV ? 'http://localhost:8888/.netlify/functions' : '/.netlify/functions';
+
 const httpClient = axios.create({
-  baseURL: 'http://localhost:8888/.netlify/functions',
+  baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
