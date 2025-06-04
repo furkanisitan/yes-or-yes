affbe569efd6a562e685c375917f4fcc56a7719b
Furkan Işıtan <furkanisitandev@gmail.com>
2025-06-04 23:14:41 +0300

    Add safelist for Tailwind CSS to include additional utility classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,30 @@ export default {
     extend: {},
   },
   plugins: [],
-}
+  safelist: [
+    // 🎨 Background gradients
+    'bg-gradient-to-tr',
+    'from-orange-400',
+    'via-white',
+    'to-yellow-300',
 
+    // 🎨 Background colors
+    'bg-yellow-200',
+
+    // 🎨 Text colors
+    'text-red-600',
+    'text-red-700',
+    'text-gray-700',
+    'text-white',
+
+    // 🅰️ Font weights
+    'font-normal',
+    'font-medium',
+    'font-semibold',
+    'font-bold',
+    'md:font-bold',
+
+    // 🔠 Font sizes
+    'text-lg',
+  ]
+}
